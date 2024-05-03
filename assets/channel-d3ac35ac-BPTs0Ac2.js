5840d3aca98856e4bac8baa1db6306280172e9f0
Divansh369 <divanshthebest@gmail.com>
2024-05-03 12:03:07 +0530

    Updates

--- assets/channel-d3ac35ac-BPTs0Ac2.js
+++
@@ -1,1 +0,0 @@
-import{aB as o,aC as r}from"./slidev/12-Br0Ms7Cr.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
---
+++ assets/channel-d3ac35ac-CSNot_O7.js
@@ -0,0 +1,1 @@
+import{aB as o,aC as r}from"./slidev/12-BxOMF0QR.js";const s=(a,n)=>o.lang.round(r.parse(a)[n]),e=s;export{e as c};
